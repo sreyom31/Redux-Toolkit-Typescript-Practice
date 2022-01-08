c289f0f9603d9d90cc43eb095675402b38b371e0
sreyom31 <sreyom31@gmail.com>
2022-01-08 18:38:10 +0530

    counter slice
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/redux-toolkit.ts
+++ src/redux-toolkit.ts
@@ -67,3 +67,14 @@ const selectedTodoSlice = createSlice({
     }
   });
   
+const counterSlice = createSlice({
+    name: "counter",
+    initialState: 0,
+    reducers: {},
+    extraReducers: {
+      [todosSlice.actions.create.type]: state => state + 1,
+      [todosSlice.actions.edit.type]: state => state + 1,
+      [todosSlice.actions.toggle.type]: state => state + 1,
+      [todosSlice.actions.remove.type]: state => state + 1
+    }
+});
