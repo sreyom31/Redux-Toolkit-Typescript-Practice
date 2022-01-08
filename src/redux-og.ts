a7beec2de166a08c5650d79656ef74004878b505
sreyom31 <sreyom31@gmail.com>
2022-01-08 16:03:11 +0530

    todo reducers
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/redux-og.ts
+++ src/redux-og.ts
@@ -87,3 +87,44 @@ export const selectTodoActionCreator = ({id} : {id: string}) : SelectTodoActionT
         }
     }
 }
+
+
+// Reducers
+const todosInitialState: Todo[] = [
+    {
+        id: uuid(),
+        desc: "Learn React",
+        isComplete: true
+    },
+    {
+        id: uuid(),
+        desc: "Learn Redux",
+        isComplete: true
+    },
+    {
+        id: uuid(),
+        desc: "Learn Redux-ToolKit",
+        isComplete: false
+    }
+];
+
+type TodoActionTypes = CreateTodoActionType | EditTodoActionType | ToggleTodoActionType | DeleteTodoActionType
+const todosReducer = (state: Todo[] = todosInitialState, action: TodoActionTypes) => {
+    switch (action.type) {
+        case CREATE_TODO: {
+            return [...state, action.payload]
+        }
+        case EDIT_TODO: {
+            return state.map(todo => todo.id === action.payload.id ? {...todo, desc: action.payload.desc} : todo)
+        }
+        case TOGGLE_TODO: {
+            return state.map(todo => todo.id === action.payload.id ? {...todo, isComplete: action.payload.isComplete} : todo)
+        }
+        case DELETE_TODO: {
+            return state.filter(todo => todo.id !== action.payload.id)
+        }
+        default: {
+            return state
+        }
+    }
+}
